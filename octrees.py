61d168ad5559f38106d14ab93a09db4668e9a565
Uladzislau Khamkou <kham@tuta.io>
2022-02-26 20:59:09 +0300

    move node findind into separate function

--- octrees.py
+++ octrees.py
@@ -4,11 +4,9 @@ class OctreeNode:
     def __init__(self):
         self.children = set()
 
-    def push(self, minimum, supremum, point):
+    def find_node(self, minimum, supremum, point):
         if isinstance(self.children, set):
-            self.children.add(point)
-            self.update_depth(minimum, supremum)
-            return
+            return self
         x, y, z = point
         x0, y0, z0 = minimum
         x2, y2, z2 = supremum
@@ -16,7 +14,12 @@ class OctreeNode:
         child = (int(x >= x1), int(y >= y1), int(z >= z1))
         x0, y0, z0 = [x0, x1][child[0]], [y0, y1][child[1]], [z0, z1][child[2]]
         x2, y2, z2 = [x1, x2][child[0]], [y1, y2][child[1]], [z1, z2][child[2]]
-        self.children[child].push((x0, y0, z0), (x2, y2, z2), (x, y, z))
+        return self.children[child].find_node((x0, y0, z0), (x2, y2, z2), point)
+
+    def push(self, minimum, supremum, point):
+        node = self.find_node(minimum, supremum, point)
+        node.children.add(point)
+        node.update_depth(minimum, supremum)
 
     def update_depth(self, minimum, supremum):
         if len(self.children) <= 8:
@@ -27,16 +30,8 @@ class OctreeNode:
             self.push(minimum, supremum, point)
 
     def contains(self, minimum, supremum, point):
-        if isinstance(self.children, set):
-            return point in self.children
-        x, y, z = point
-        x0, y0, z0 = minimum
-        x2, y2, z2 = supremum
-        x1, y1, z1 = ((x0 + x2) // 2 for x0, x2 in zip(minimum, supremum))
-        child = (int(x >= x1), int(y >= y1), int(z >= z1))
-        x0, y0, z0 = [x0, x1][child[0]], [y0, y1][child[1]], [z0, z1][child[2]]
-        x2, y2, z2 = [x1, x2][child[0]], [y1, y2][child[1]], [z1, z2][child[2]]
-        return self.children[child].contains((x0, y0, z0), (x2, y2, z2), point)
+        node = self.find_node(minimum, supremum, point)
+        return point in node.children
 
     def __iter__(self):
         if isinstance(self.children, set):
