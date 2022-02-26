f9f4478abbe30266c8988efc5b7148a6d2ed25c2
Uladzislau Khamkou <kham@tuta.io>
2022-02-27 00:11:52 +0300

    implement dictionary using octree

--- octrees.py
+++ octrees.py
@@ -65,3 +65,79 @@ class Octree:
 
     def __iter__(self):
         yield from self.root
+
+
+from dataclasses import dataclass
+
+@dataclass
+class Point:
+    x: int
+    y: int
+    z: int
+    def __add__(u, v):
+        return Point(*(x + y for x, y in zip(u, v)))
+    def __sub__(u, v):
+        return Point(*(x - y for x, y in zip(u, v)))
+    def __floordiv__(u, alpha):
+        return Point(*(x // alpha for x in u))
+    def __iter__(self):
+        yield from (self.x, self.y, self.z)
+
+import enum
+class Nodes(enum.Enum):
+    empty = 1
+    inner = 2
+    leaf = 3
+
+import arenas
+class OctreeDict:
+    def __init__(self, minimum, supremum):
+        self.minimum = Point(*minimum)
+        self.supremum = Point(*supremum)
+        self.inodes = arenas.Arena()
+        self.leafs = arenas.Arena()
+        self.root = [Nodes.empty, None]
+
+    def __setitem__(self, p, value):
+        if not isinstance(p, Point):
+            p = Point(*p)
+        x0, x2 = self.minimum, self.supremum
+        node = self.root
+        while node[0] is not Nodes.empty:
+            x1 = (x2 + x0) // 2
+            child = tuple(int(x >= y) for x, y in zip(p, x1))
+            if node[0] is Nodes.leaf:
+                if self.leafs[node[1]][0] == p:
+                    self.leafs[node[1]][1] = value
+                    return
+                node[0] = Nodes.inner
+                leaf1 = node[1]
+                p1 = self.leafs[leaf1][0]
+                child1 = tuple(int(x >= y) for x, y in zip(p1, x1))
+                node[1] = self.inodes.push({child1: [Nodes.leaf, leaf1]})
+            children = self.inodes[node[1]]
+            if child not in children:
+                children[child] = [Nodes.empty, None]
+            x0 = Point(*([y0, y1][b] for y0, y1, b in zip(x0, x1, child)))
+            x2 = Point(*([y0, y1][b] for y0, y1, b in zip(x1, x2, child)))
+            node = children[child]
+        node[0] = Nodes.leaf
+        node[1] = self.leafs.push([p, value])
+
+    def __contains__(self, p):
+        if not isinstance(p, Point):
+            p = Point(*p)
+        x0, x2 = self.minimum, self.supremum
+        node = self.root
+        while node[0] is Nodes.inner:
+            x1 = (x2 + x0) // 2
+            child = tuple(int(x >= y) for x, y in zip(p, x1))
+            children = self.inodes[node[1]]
+            if child not in children:
+                children[child] = [Nodes.empty, None]
+            node = children[child]
+        return node[0] is Nodes.leaf
+
+    def __iter__(self):
+        for leaf in self.leafs:
+            yield leaf
