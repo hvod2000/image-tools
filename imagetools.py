acb3e3a3bf55397808276279fce9b8cd35bdce88
Uladzislau Khamkou <kham@tuta.io>
2022-02-26 16:32:33 +0300

    prettify code

--- imagetools.py
+++ imagetools.py
@@ -2,11 +2,14 @@ import PIL.Image, PIL.ImageDraw
 from math import inf, dist
 from kmeans_clustering import kmeans
 
+
 def number2hex(number, size=2):
-    return hex(number)[2:].rjust(2, '0')
+    return hex(number)[2:].rjust(2, "0")
+
 
 def color2hex(color):
-    return ''.join(map(number2hex, color))
+    return "".join(map(number2hex, color))
+
 
 class Image:
     def __init__(self, content, palette=None):
@@ -36,16 +39,20 @@ class Image:
         result = PIL.Image.new("RGB", size)
         draw = PIL.ImageDraw.Draw(result)
         old_pixels = self.content.load()
-        radius = min(radius, min(int(self.size[i] / result.size[i] / 2 - 0.5) for i in range(2)))
+        resize_ratio = min(c / r for c, r in zip(self.size, size))
+        radius = min(radius, int((resize_ratio - 1) / 2))
         for y in range(result.size[1]):
             y_old = int((y + 0.5) * self.size[1] / result.size[1])
             for x in range(result.size[0]):
                 x_old = int((x + 0.5) * self.size[0] / result.size[0] + 0.5)
-                colors = []
+                color_sum = [0] * len(old_pixels[0, 0])
                 for dy in range(-radius, radius + 1):
                     for dx in range(-radius, radius + 1):
-                        colors.append(old_pixels[x_old + dx, y_old + dy])
-                new_color = tuple(round(sum(c[i] for c in colors) / len(colors)) for i in range(len(colors[0])))
+                        color = old_pixels[x_old + dx, y_old + dy]
+                        for i in range(len(color)):
+                            color_sum[i] += color[i]
+                color_count = (radius * 2 + 1) ** 2
+                new_color = tuple(round(c / color_count) for c in color_sum)
                 draw.point((x, y), new_color)
         return Image(result, self.palette)
 
@@ -89,15 +96,18 @@ class Image:
 
     def create_palette_using_kmeans(self, palette_size):
         import random
+
         colors = []
         pixels = self.content.load()
         for y in range(self.size[1]):
             for x in range(self.size[0]):
                 colors.append(pixels[x, y])
         random.shuffle(colors)
-        return {f'color{number}': color for number, color in enumerate(tuple(max(min(round(x), 255), 0) for x in color) for color in kmeans(colors, palette_size))}
+        colors = kmeans(colors, palette_size)
+        colors = map(lambda c: tuple(map(round, c)), colors)
+        return {f"color{number}": color for number, color in enumerate(colors)}
 
-    def create_palette(self, palette_size = None):
+    def create_palette(self, palette_size=None):
         if palette_size:
             return self.create_palette_using_kmeans(palette_size)
         colors = set()
@@ -105,23 +115,23 @@ class Image:
         for y in range(self.size[1]):
             for x in range(self.size[0]):
                 colors.add(pixels[x, y])
-        return {f'color{i}':c for i, c in enumerate(colors)}
+        return {f"color{i}": c for i, c in enumerate(sorted(colors))}
 
     def to_css(self):
         assert self.palette
         colors = list(sorted(self.palette.items()))
         result = []
         for color_name, color in colors:
-            result.append(f'table.pixelart *.{color_name} ' + '{')
-            result.append(f'\tbackground-color: #{color2hex(color)};')
-            result.append('}')
-        return '\n'.join(result)
+            result.append(f"table.pixelart *.{color_name} " + "{")
+            result.append(f"\tbackground-color: #{color2hex(color)};")
+            result.append("}")
+        return "\n".join(result)
 
     def to_html(self):
         assert self.palette
-        color2name = {rgb: color_name for color_name, rgb in self.palette.items()}
+        color2name = {color: name for name, color in self.palette.items()}
         pixels = self.content.load()
-        result = ['<table class=pixelart>']
+        result = ["<table class=pixelart>"]
         for y in range(self.size[1]):
             row_color = color2name[pixels[0, y]]
             result.append(f'\t<tr class="{row_color}">')
@@ -130,10 +140,10 @@ class Image:
                 if color != row_color:
                     result.append(f'\t\t<td class="{color}" />')
                 else:
-                    result.append(f'\t\t<td/>')
-            result.append('\t</tr>')
-        result.append('</table>')
-        return '\n'.join(result)
+                    result.append(f"\t\t<td/>")
+            result.append("\t</tr>")
+        result.append("</table>")
+        return "\n".join(result)
 
     def __iter__(self):
         pixels = self.content.load()
