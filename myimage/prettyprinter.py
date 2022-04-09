196355bb150c6d53ebb135ccd6a8d3a890cdfe5e
Uladzislau Khamkou <kham@tuta.io>
2022-04-09 21:22:58 +0300

    use each character to print two pixels

--- myimage/prettyprinter.py
+++ myimage/prettyprinter.py
@@ -1,12 +1,18 @@
 __all__ = ["show"]
 
-def colored(r, g, b, text):
-    return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
+
+def str_column(lower, upper):
+    r0, g0, b0 = lower
+    r1, g1, b1 = upper
+    result = "\033[38;2;{};{};{}m".format(r1, g1, b1)
+    result += "\033[48;2;{};{};{}m".format(r0, g0, b0)
+    result += "▀"
+    result += "\033[0m"
+    return result
 
 
 def show(image):
-    for y in range(image.size - 1, -1, -1):
+    for y in range((image.size - 1) // 2 * 2 - 1, -1, -2):
         for x in range(image.size):
-            r, g, b = image[x, y]
-            print(end=colored(r, g, b, "#"))
+            print(end=str_column(image[x, y], image[x, y + 1]))
         print()
