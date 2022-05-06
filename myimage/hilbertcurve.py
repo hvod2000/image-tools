5e01f38ea7fe9a1a0d4bd2a7a224d71cb77d040b
Uladzislau Khamkou <kham@tuta.io>
2022-05-06 18:55:55 +0300

    implement Hilbert curve

---
+++ myimage/hilbertcurve.py
@@ -0,0 +1,13 @@
+def hilbert(level):
+    path, rules = "A", { "A": "+BF-AFA-FB+", "B": "-AF+BFB+FA-"}
+    for _ in range(level):
+        path = "".join(rules.get(ch, ch) for ch in path)
+    pos, drn = (0, 0), (1, 0)
+    yield pos
+    for cmd in path:
+        match cmd:
+            case rotation if rotation in "+-":
+                drn = (-drn[1], drn[0]) if rotation == '+' else (drn[1], -drn[0])
+            case 'F':
+                pos = tuple(map(sum, zip(pos, drn)))
+                yield pos
