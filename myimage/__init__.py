6309e2ba0f4225fc98c956295f6890732dd4fc17
Uladzislau Khamkou <kham@tuta.io>
2022-05-18 20:01:07 +0300

    make method binding more flexible

--- myimage/__init__.py
+++ myimage/__init__.py
@@ -1,14 +1,15 @@
-import inspect
-from functools import partial
+from pathlib import Path
 from .myimage import Image
-from . import prettyprinter, filemanager, quadtree, dithering
-
-for methods in [prettyprinter, filemanager, quadtree, dithering]:
-    for name, f in methods.__dict__.items():
+from importlib import import_module
+import inspect
+IGNORED_SUBMODULES = {"__init__", __package__}
+for f in Path(__file__).resolve().parent.iterdir():
+    if f.suffix != ".py" or not f.is_file() or f.stem in IGNORED_SUBMODULES:
+        continue
+    for name, f in import_module("." + f.stem, __package__).__dict__.items():
         if not callable(f):
             continue
-        arg = next(iter(inspect.signature(f).parameters.keys()), None)
-        if arg in ("self", "image"):
-            setattr(Image, name, f)
-        else:
-            setattr(Image, name, staticmethod(f))
+        parameters = inspect.signature(f).parameters.keys()
+        if next(iter(parameters), None) not in ("self", "image"):
+            f = staticmethod(f)
+        setattr(Image, name, f)
